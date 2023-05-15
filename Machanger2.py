9a41dc9f28d55b8da41d35db3987ca1b7168286f
sar1 <129823338+sar369@users.noreply.github.com>
2023-05-15 15:49:19 +0530

    Update Machanger2.py

--- Machanger2.py
+++ Machanger2.py
@@ -9,8 +9,8 @@ interface = input("Your interface: ")
 new_mac = input("Enter New Mac address: ")
 
 print("[+] Changing Mac address for " + interface + " to " + new_mac)
-subprocess.call("ifconfig ", interface, " down")
-subprocess.call("ifconfig ", interface, " hw", "ether ",  new_mac)
-subprocess.call("ifconfig ", interface, " up")
+subprocess.call(["ifconfig ", interface, " down"])
+subprocess.call(["ifconfig ", interface, " hw", "ether ",  new_mac])
+subprocess.call(["ifconfig ", interface, " up"])
 
 
